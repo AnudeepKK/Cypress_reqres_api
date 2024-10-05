f0b2c6933f573f94d8189e750d118eb8a6c60ef2
Anudeep K K <102415263+AnudeepKK@users.noreply.github.com>
2024-10-05 11:30:40 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,10 @@
 const { defineConfig } = require("cypress");
-const mochawesome = require("mochawesome");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Adding mochawesome reporter for generating reports
-      require("cypress-mochawesome-reporter/plugin")(on);
-      return config;
+      return config
     },
-    specPattern: "cypress/integration/APItest/*.cy.js", // Corrected path with single slashes
+    specPattern:"cypress//Integration//APItest//*.cy.js"
   },
 });
